adb94a8a78df8b14f601a2ebe92ae50c49816246
Håkon Teigen Lund <hakon.lund@bekk.no>
2025-11-16 15:11:44 +0100

    Changed some of the text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,13 @@ export const Home = () => {
     <div className="home">
       <h1>Håkons arrangementer</h1>
       <p>
-        Jeg heter Håkon Teigen Lund, og jeg jobber med å arrangere sanger for kor – alt fra poplåter til klassiske perler.
+        Jeg heter Håkon Teigen Lund, og jeg jobber med å arrangere sanger for kor, alt fra poplåter til klassiske stykker.
         Jeg har arrangert for kor i snart femten år, og har også dirigert flere kor gjennom årene. Det gir meg et godt
-        blikk for hva som fungerer i praksis – både musikalsk og i øvingshverdagen.
+        blikk for hva som fungerer i praksis.
       </p>
       <p>
         Her på nettsiden finner du en liste over ferdige arrangementer som du kan bruke med koret ditt. Ønsker du noe helt eget,
-        lager jeg gjerne spesialtilpassede arrangementer – og jeg tilpasser alltid etter korets nivå og behov.
+        lager jeg gjerne spesialtilpassede arrangementer.
       </p>
       <p>
         Bare send meg en e-post på <a href="mailto: hakontei@gmail.com">hakontei@gmail.com</a> hvis du vil bestille nytt arrangement, kjøpe et av mine ferdige arrangement, eller har spørsmål. Håper du finner noe du liker – og takk for at du titter innom!
